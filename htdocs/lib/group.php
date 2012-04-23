5b91e40b7fad0ac8e2899a0b1e8ca66c9f15bcde
Richard Mansfield <richard.mansfield@catalyst.net.nz>
2012-04-23 18:44:02 +1200

    Use clean group urls on find/my groups
    Change-Id: I325998b762dde6880678df556f0f200a6d72bf4d
    Signed-off-by: Richard Mansfield <richard.mansfield@catalyst.net.nz>

--- htdocs/lib/group.php
+++ htdocs/lib/group.php
@@ -1843,11 +1843,11 @@ function group_get_associated_groups($userid, $filter='all', $limit=20, $offset=
     
     $sql = '
         SELECT g1.id, g1.name, g1.description, g1.public, g1.jointype, g1.request, g1.grouptype, g1.submittableto,
-            g1.hidemembers, g1.hidemembersfrommembers, g1.membershiptype, g1.reason, g1.role, g1.membercount,
+            g1.hidemembers, g1.hidemembersfrommembers, g1.urlid, g1.membershiptype, g1.reason, g1.role, g1.membercount,
             COUNT(gmr.member) AS requests
         FROM (
             SELECT g.id, g.name, g.description, g.public, g.jointype, g.request, g.grouptype, g.submittableto,
-                g.hidemembers, g.hidemembersfrommembers, t.membershiptype, t.reason, t.role,
+                g.hidemembers, g.hidemembersfrommembers, g.urlid, t.membershiptype, t.reason, t.role,
                 COUNT(gm.member) AS membercount
             FROM {group} g
             LEFT JOIN {group_member} gm ON (gm.group = g.id)' .
@@ -1855,11 +1855,11 @@ function group_get_associated_groups($userid, $filter='all', $limit=20, $offset=
             WHERE g.deleted = ?' .
             $catsql . '
             GROUP BY g.id, g.name, g.description, g.public, g.jointype, g.request, g.grouptype, g.submittableto,
-                g.hidemembers, g.hidemembersfrommembers, t.membershiptype, t.reason, t.role
+                g.hidemembers, g.hidemembersfrommembers, g.urlid, t.membershiptype, t.reason, t.role
         ) g1
         LEFT JOIN {group_member_request} gmr ON (gmr.group = g1.id)
         GROUP BY g1.id, g1.name, g1.description, g1.public, g1.jointype, g1.request, g1.grouptype, g1.submittableto,
-            g1.hidemembers, g1.hidemembersfrommembers, g1.membershiptype, g1.reason, g1.role, g1.membercount
+            g1.hidemembers, g1.hidemembersfrommembers, g1.urlid, g1.membershiptype, g1.reason, g1.role, g1.membercount
         ORDER BY g1.name';
 
     $groups = get_records_sql_array($sql, $values, $offset, $limit);
@@ -2170,4 +2170,14 @@ function group_get_new_homepage_urlid($desired) {
         $newname = substr($desired, 0, $maxlen - strlen($i) - 1) . '-' . $i;
     }
     return $newname;
+}
+
+function group_homepage_url($group, $useid=false) {
+    if (!$useid && !is_null($group->urlid) && get_config('cleanurls')) {
+        return get_config('cleanurlgroupdefault') . '/' . $group->urlid;
+    }
+    if ($group->id) {
+        return 'group/view.php?id=' . $group->id;
+    }
+    throw new SystemException("group_homepage_url called with no group id");
 }
